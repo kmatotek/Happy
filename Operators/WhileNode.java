86405e860f11c5b2047db900cab73e71f863d5ea
Kadin Matotek <39473540+kmatotek@users.noreply.github.com>
2024-11-06 21:06:39 -0500

    Update WhileNode.java

--- Operators/WhileNode.java
+++ Operators/WhileNode.java
@@ -6,11 +6,13 @@ public class WhileNode extends ASTNode {
     public ASTNode bodyNode;
     public Position posStart;
     public Position posEnd;
+    public boolean shouldReturnNull;
 
-    public WhileNode(ASTNode conditionNode, ASTNode bodyNode){
+    public WhileNode(ASTNode conditionNode, ASTNode bodyNode, boolean shouldReturnNull){
         this.conditionNode = conditionNode;
         this.bodyNode = bodyNode;
         this.posStart = conditionNode.positionStart;
         this.posEnd = bodyNode.positionEnd;
+        this.shouldReturnNull = shouldReturnNull;
     }
 }
